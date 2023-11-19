8629fab406dc62ef489f07fbb740114dae007e5c
23kathi <katherinne.sanchez.segura@est.una.ac.cr>
2023-11-19 13:42:36 -0600

    Archivo validators.py , contiene funciones para valudar tipo de dato y retorno

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hola")
+from scopestack import ScopeStack
+from symboltable import SymbolTable, Symbol
+from errorstack import ErrorStack
+import re
+from reutils import FUNCTION_DECLARATION_RE, VARIABLE_DECLARATION_RE, VARIABLE_ASSIGNATION_RE, CONDITIONS_RE, RETURN_RE
+from validators import valid_return_type, validate_data_type
+
+symbol_table = SymbolTable()
+error_stack = ErrorStack()
+scope_stack = ScopeStack()
